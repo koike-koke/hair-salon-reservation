54ed1dcec7489535b905a2a6e948c9e51e5ff9b0
koike <koike@koike2gou>
2025-12-24 13:52:26 +0900

    first commit

---
+++ src/main/java/com/udemy/spring3Item/HairSalonReservationApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring3Item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairSalonReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairSalonReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/spring3Item/HairSalonReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring3Item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairSalonReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
